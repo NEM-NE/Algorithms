b79a1a18090adee557f91897a573009f0ad04a15
NEMNE <dlatqdlatq@naver.com>
2021-03-22 00:03:38 +0900

    0322 DP 10844번 풀고 있는 중..

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 12 : 16
+	// 10 : 38
 	
 	static long[] dp = new long[101];
 	
@@ -13,18 +13,11 @@ public class Main {
 
 		int tc = scanner.nextInt();
 		dp[1] = 9;
-		
-		for(int i = 2; i < tc+1; i++) {
-			if(i%2 == 0) {
-				dp[i] = 2 * dp[i-1] - 1;
-				dp[i] %= 1000000000;
-			}else {
-				dp[i] = 2 * (dp[i-1] - 1);
-				dp[i] %= 1000000000;	
-			}
-
+		dp[2] = 17;
+		for(int i = 3; i < tc+1; i++) {
+			dp[i] = (long) (2 * dp[i-1] - (i-1));
+			dp[i] %= 1000000000;
 		}
-		
 		System.out.println(dp[tc]);
 		
 	}
