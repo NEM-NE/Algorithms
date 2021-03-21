16f8d9567d3a94dc7b560fc1c49ae61cdcc6b722
NEMNE <dlatqdlatq@naver.com>
2021-03-21 15:13:06 +0900

    [0320] 다이나믹 프로그래밍 알고리즘 풀이

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,32 +4,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10 : 33
+	// 12 : 16
+	
+	static long[] dp = new long[101];
+	
 	public static void main(String[] args) throws IOException {		
 		Scanner scanner = new Scanner(System.in);
 
 		int tc = scanner.nextInt();
-
-		for(int i = 0; i < tc; i++) {
-			for(int j = tc-i-1; j > 0; j-- ) {
-				System.out.print(" ");
-			}
-			
-			if(i == tc-1) {
-				for(int j = 0; j < 2*i+1; j++) {
-					System.out.print("*");
-				}
-			}else if(i == 0){
-				System.out.print("*");
+		dp[1] = 9;
+		
+		for(int i = 2; i < tc+1; i++) {
+			if(i%2 == 0) {
+				dp[i] = 2 * dp[i-1] - 1;
+				dp[i] %= 1000000000;
 			}else {
-				System.out.print("*");
-				for(int j = 0; j < 2 * i -1; j++) {
-					System.out.print(" ");
-				}
-				System.out.print("*");
+				dp[i] = 2 * (dp[i-1] - 1);
+				dp[i] %= 1000000000;	
 			}
-			
-			System.out.println();
+
 		}
+		
+		System.out.println(dp[tc]);
+		
 	}
 }
