30c00187d50ba6393d8d89008829c2d7b22f6b8d
NEMNE <dlatqdlatq@naver.com>
2021-03-26 23:55:45 +0900

    0326 DP programming solving....

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,40 +4,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 41
+	// 11 : 35
 	
-	static long[][] dp = new long[101][10];
+	static long[][] dp = new long[100000][2];
 	
 	public static void main(String[] args) throws IOException {		
 		Scanner scanner = new Scanner(System.in);
 
 		int tc = scanner.nextInt();
 		
-		// 1
-		for(int i = 1; i < 10; i++) {
-			dp[1][i] = 1;
-		}
-		
-		for(int i = 2; i < tc + 1; i++) {
-			for(int j = 0; j < 10; j++) {
-				if(j == 0) {
-					dp[i][j] = dp[i-1][j+1]%1000000000;
-				}else if(j == 9) {
-					dp[i][j] = dp[i-1][j-1]%1000000000;
-				}else {
-					dp[i][j] = dp[i-1][j+1]%1000000000 + dp[i-1][j-1]%1000000000;
-				}
-		
+		for(int i = 0; i < tc; i++) {
+			//init
+			int column = scanner.nextInt();
+			for(int k = 0; k < column; k++) {
+				dp[k][0] = scanner.nextInt();
 			}
+			for(int k = 0; k < column; k++) {
+				dp[k][1] = scanner.nextInt();
+			}
+			
+			
 		}
 		
-		int sum = 0;
-		for(int i = 0; i < 10; i++) {
-			sum += dp[tc][i]%1000000000;
-			sum %= 1000000000;
-		}
-		
-		System.out.println(sum);
 		
 	}
 	
