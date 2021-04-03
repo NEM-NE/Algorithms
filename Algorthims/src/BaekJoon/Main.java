dbda58cb568dddcc9fec59c22adbda8476dfed20
NEMNE <dlatqdlatq@naver.com>
2021-04-03 18:33:07 +0900

    DP Programming 2133 solve

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,35 +4,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 09
+	// 10 : 15
 	
-	static int[] dp = new int[100001];
+	static long[] dp = new long[100001];
 	
 	public static void main(String[] args) throws IOException {		
 		Scanner scanner = new Scanner(System.in);
 
 		int tc = scanner.nextInt();
-		for(int i = 1; i < tc+1; i++) {
-			dp[i] = i;
-		}
-
-		for(int i = 1; i < tc+1; i++) {
-			if(i % 2 == 0) {
-				for(int j = 1; j <= i/2; j++) {
-					dp[i] = Math.min(dp[i], dp[j] + dp[i-j]);	
-				}	
-			}else {
-				for(int j = 1; j < i/2; j++) {
-					dp[i] = Math.min(dp[i], dp[j] + dp[i-j]);	
-				}	
-			}
-			double num = Math.sqrt(i);
-			if(num - (int) num == 0 ) {
-				dp[i] = 1;
-			}
-			
+		dp[2] = 3;
+		dp[4] = 11;
+		for(int i = 6; i < tc+1; i += 2) {
+			dp[i] = dp[i-2] * dp[2] + 2;
+			dp[i] += (i-6)/2 * 4;
+			if((i/2) % 2 ==0) dp[i] -= 1;
 		}
 		
 		System.out.println(dp[tc]);
+
 	}
 }
