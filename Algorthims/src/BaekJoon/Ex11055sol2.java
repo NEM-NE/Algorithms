baa808abccd13d6e804154fe577a84a981450c84
NEMNE <dlatqdlatq@naver.com>
2021-04-06 00:12:08 +0900

    0405 DP 11055 11054 11052

--- Algorthims/src/BaekJoon/Ex11055sol2.java
+++ Algorthims/src/BaekJoon/Ex11055sol2.java
@@ -3,8 +3,8 @@ package BaekJoon;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	// 12 : 01
+public class Ex11055sol2 {
+	// 10 : 54
 	
 	static int[] dp = new int[1001];
 	
@@ -12,19 +12,28 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 
 		int tc = scanner.nextInt();
-		
 		int[] ary = new int[tc+1];
+		
 		for(int i = 1; i < tc+1; i++) {
 			ary[i] = scanner.nextInt();
 		}
 		
-		dp[1] = ary[1];
-		
+
 		for(int i = 1; i < tc+1; i++) {
-			for(int j = 1; j <= i; j++) {
-				
+			for(int j = 1; j < i; j++) {
+				if(ary[i] > ary[j]) {
+					dp[i] = Math.max(dp[i], dp[j]);
+				}
 			}
+			dp[i] += ary[i];
 		}
+		
+		int max = 0;
+		for(int i = 1; i < tc+1; i++) {
+			if(max < dp[i]) max = dp[i];
+		}
+		
+		System.out.println(max);
 		
 	}
 }
--- Algorthims/src/BaekJoon/No2225_sumDiv.java
+++ Algorthims/src/BaekJoon/No2225_sumDiv.java
@@ -1,31 +1,21 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 public class No2225_sumDiv {
-static long dp[][] = new long[201][201];
-public static void main(String[] args) throws Exception{
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-String tc = br.readLine();
-StringTokenizer st = new StringTokenizer(tc, " ");
-int n = Integer.parseInt(st.nextToken());
-int k = Integer.parseInt(st.nextToken());
-for(int i=0; i<201; i++){
-dp[i][0] = 1;
-dp[1][i] = 1;
-}
-for(int i=1; i<201; i++){
-dp[2][i] = i+1;
-}
-for(int i=3; i<201; i++){
-for(int j=1; j<201; j++){
-for(int k1=0; k1<=j; k1++){
-dp[i][j]+= dp[i-1][j-k1]%1000000000;
-}
-}
-}
-System.out.println(dp[k][n]%1000000000);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int mod = 1000000;
+        int[] dp = new int[input.length() + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= input.length(); i++) {
+            int now = i - 1;
+            if (input.charAt(now) > '0')
+               dp[i] = dp[i - 1];
+            int su = (input.charAt(now - 1) - '0') * 10 + (input.charAt(now) - '0');
+           if (su >= 10 && su <= 26)
+                dp[i] = (dp[i] + dp[i - 2]) % mod;
+        }
+       System.out.println(input.equals("0") ? 0 : dp[input.length()]);
+    }
 }
