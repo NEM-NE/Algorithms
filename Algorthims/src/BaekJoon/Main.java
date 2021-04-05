500aafa8cf3768183d9e2b9a62f610f025043963
NEMNE <dlatqdlatq@naver.com>
2021-04-05 17:32:15 +0900

    0405 DP 9461, 2225, 2011 solve

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,23 +4,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10 : 15
+	// 4 : 50
 	
-	static long[] dp = new long[100001];
+	static long[] dp = new long[5000];
 	
 	public static void main(String[] args) throws IOException {		
 		Scanner scanner = new Scanner(System.in);
 
-		int tc = scanner.nextInt();
-		dp[2] = 3;
-		dp[4] = 11;
-		for(int i = 6; i < tc+1; i += 2) {
-			dp[i] = dp[i-2] * dp[2] + 2;
-			dp[i] += (i-6)/2 * 4;
-			if((i/2) % 2 ==0) dp[i] -= 1;
+		String tc = scanner.next();
+		
+		int[] ary = new int[tc.length()+1];
+		for(int i = 1; i < tc.length()+1; i++) {
+			ary[i] = tc.charAt(i-1) - 48;
 		}
 		
-		System.out.println(dp[tc]);
+		dp[0] = 1;
+		if(ary[1] != 0) {
+			dp[1] = 1;
+		}else {
+			System.out.println(0);
+			System.exit(0);
+		}
+		
+		for(int i = 2; i < ary.length; i++) {
+			if(10*ary[i-1] + ary[i] > 27 || ary[i] == 0) {
+				if(ary[i] == 0 && (10*ary[i-1] + ary[i] > 27 || 10*ary[i-1] + ary[i] == 0)) {
+					System.out.println(0);
+					System.exit(0);
+				}
+				dp[i] = dp[i-1];
+				dp[i] %= 1000000;
+			}else {
+				dp[i] = dp[i-1] + dp[i-2];
+				dp[i] %= 1000000;
+			}
+		}
+		
+		System.out.println(dp[ary.length-1]);
+		
 
 	}
 }
