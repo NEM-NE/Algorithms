21fa097eeb605ead5dea5ba63b7fac4d7a7a4616
NEMNE <dlatqdlatq@naver.com>
2021-04-09 23:36:35 +0900

    0410 DP 1699, 2133

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,28 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10: 23
+	// 11 : 23
 	
-	static int[] dp = new int[301];
+	static int[] dp = new int[31];
 	
 	public static void main(String[] args) throws IOException {		
 		Scanner scanner = new Scanner(System.in);
 
 		int tc = scanner.nextInt();
-		int[] ary = new int[tc+1];
 		
-		for(int i = 1; i < tc+1; i++) {
-			ary[i] = scanner.nextInt();
+		for(int i = 2; i < tc+1; i++) {
+			
 		}
-		
-		dp[1] = ary[1];
-		if(tc > 1)dp[2] = ary[1] + ary[2];
-		
-		for(int i = 3; i < tc+1; i++) {
-			dp[i] = Math.max(dp[i-2], dp[i-3]+ary[i-1]) + ary[i];
-		}
-		
-		System.out.println(dp[tc]);
-		
 	}
 }
