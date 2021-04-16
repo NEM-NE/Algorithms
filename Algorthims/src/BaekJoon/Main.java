4d32660ecbf4feff1243070adca39587e4e7851e
NEMNE <dlatqdlatq@naver.com>
2021-04-17 00:01:27 +0900

    0417 Sorting solve & study

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -1,36 +1,28 @@
-package BaekJoon;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 07
-		static int[] dp = new int[5001];
+	// 11 : 36
 		
 	public static void main(String[] args) throws IOException {		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner scanner = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
-
-		int tc = Integer.parseInt(br.readLine());
 		
-		int[] ary = new int[tc+1];
-		int[] cnt = new int[2000001];
-		for(int i = 0; i < tc; i++) {
-			ary[i] = Integer.parseInt(br.readLine());
-		}
+		int tc = scanner.nextInt();
+		LinkedList<Integer> list = new LinkedList<Integer>();
 		
 		for(int i = 0; i < tc; i++) {
-			cnt[ary[i] + 1000000]++;
+			list.add(scanner.nextInt());
 		}
 		
-		for(int i = 0; i < cnt.length; i++) {
-			if(cnt[i] == 1) sb.append(i-1000000 + "\n");
+		Collections.sort(list);
+		
+		for(int i = 0; i < tc; i++) {
+			sb.append(list.get(i)).append('\n');
 		}
+		
 		
 		System.out.println(sb);
-
-
-
 		
 	}
 }
