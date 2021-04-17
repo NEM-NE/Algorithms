ae44a2e3bc6a9c5c8a8d91d075545b83eaa6c634
NEMNE <dlatqdlatq@naver.com>
2021-04-18 00:04:34 +0900

    0418 Sorting 11652 solved

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,29 +4,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 07 : 52
+	// 10 : 20
 
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int tc = Integer.parseInt(br.readLine());
-		int[] ary = new int[tc];
-		int[] cnt = new int[10001];
+		long[] list = new long[tc];
+		long[][]cnt2 = new long[tc][2]; 
 		
 		for(int i = 0; i < tc; i++) {
-			ary[i] = Integer.parseInt(br.readLine());
-			cnt[ary[i]]++;
+			list[i] = (Long.parseLong((br.readLine())));
 		}
 		
-		for(int i = 0; i < cnt.length; i++ ) {
-			for(int j = 0; j < cnt[i]; j++) {
-				sb.append(i).append('\n');
+		Arrays.sort(list);
+
+		Long cur;
+		Long pre;
+		int index = 0;
+		
+		cnt2[0][0] = 1;
+		cnt2[0][1] = list[0];
+		for(int i = 1; i < list.length; i++) {
+			pre= list[i-1];
+			cur = list[i];
+			if(cur != pre) {
+				index++;
+				cnt2[index][1] = cur;
+			}
+			cnt2[index][0]++;
+		}
+		
+		long max = 0;
+		long ans = 0;
+		for(int i = 0; i < cnt2.length; i++) {
+			if(max < cnt2[i][0]) {
+				max = cnt2[i][0];
+				ans = cnt2[i][1];
 			}
 		}
 		
-		System.out.println(sb);
-	
+		sb.append(ans).append('\n');
 		
+		System.out.println(sb);
 	}
 }
