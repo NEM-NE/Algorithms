8c9a39c7a21b05d15969ad9b2c25a02dac1f6fba
NEMNE <dlatqdlatq@naver.com>
2021-04-20 23:06:29 +0900

    0420 11652 9012 10799 10820 resolve

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,24 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10 : 27
+	// 10 : 55
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String str =br.readLine();
-		String[] ary = new String[str.length()];
 		
-		for(int i = 0; i < ary.length; i++) {
-			ary[i] = str.substring(i, str.length());
+		int tc = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < tc; i++) {
+			String str = br.readLine();
+			Stack<Character> stack = new Stack<>();
+			for(int j = 0; j < str.length(); j++) {
+				char a = str.charAt(j);
+				if(a == '(') {
+					stack.push(a);
+				}else {
+					if(stack.size() != 0) stack.pop();
+					else {
+						stack.push(a);
+						break;
+					}
+				}
+			}
+			
+			if(stack.size() == 0) System.out.println("YES");
+			else System.out.println("NO");
 		}
+
 		
-		Arrays.sort(ary);
-		
-		for(int i = 0; i < ary.length; i++) {
-			sb.append(ary[i]).append('\n');
-		}
-		
-		System.out.println(sb);
 	}
 }
