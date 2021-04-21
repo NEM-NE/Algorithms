bd78547d797eeaf2589e8ba8f37880c3f59a7507
NEMNE <dlatqdlatq@naver.com>
2021-04-21 23:40:52 +0900

    0421 Linkedlist solve...

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,34 +4,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10 : 55
+	// 10 : 46
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
+		String str = br.readLine();
+		LinkedList<Character> list = new LinkedList<>();
 		int tc = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < tc; i++) {
-			String str = br.readLine();
-			Stack<Character> stack = new Stack<>();
-			for(int j = 0; j < str.length(); j++) {
-				char a = str.charAt(j);
-				if(a == '(') {
-					stack.push(a);
-				}else {
-					if(stack.size() != 0) stack.pop();
-					else {
-						stack.push(a);
-						break;
-					}
-				}
-			}
-			
-			if(stack.size() == 0) System.out.println("YES");
-			else System.out.println("NO");
+		for(int i = 0; i < str.length(); i++) {
+			list.add(str.charAt(i));
 		}
-
+		int size = list.size();
+		int point = size;
+		for(int i = 0; i < tc; i++) {
+			String input = br.readLine();
+	
+			if(input.charAt(0) == 'P') {
+				char a = input.charAt(2);
+				list.add(point, a);
+				point++;
+				size++;
+			}else if(input.charAt(0) == 'L') {
+				if(point == 0) continue;
+				point--;
+			}else if(input.charAt(0) == 'B') {
+				if(point == 0) continue;
+				list.remove(point-1);
+				point--;
+				size--;
+			}else if(input.charAt(0) == 'D') {
+				if(point == size) continue;
+				point++;
+			}
+		}
+		
+		String ans = "";
+		for(int i = 0; i < list.size(); i++) {
+			ans += list.get(i);
+		}
+		
+		sb.append(ans);
+		
+		System.out.println(sb);
 		
 	}
 }
