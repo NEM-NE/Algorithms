f2d1509aa1fe0cb24f6c6e2e3621f8c698e8f854
NEMNE <dlatqdlatq@naver.com>
2021-04-24 23:27:20 +0900

    0424 Linkedlist 1406 1158 solve

--- Algorthims/src/BaekJoon/Ex1158.java
+++ Algorthims/src/BaekJoon/Ex1158.java
@@ -3,45 +3,36 @@ package BaekJoon;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	// 11 : 10 
-	
+public class Ex1158 {
+	// 10 : 35
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		LinkedList<Integer> list = new LinkedList<>();
+		ArrayList ans = new ArrayList();
 		
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		Queue<Integer> que = new LinkedList<>();
-		int[] ans = new int[n];
-		
 		for(int i = 1; i <= n; i++) {
-			que.add(i);
+			list.add(i);
 		}
-		int index = 0;
-		while(que.size() != 0) {
-			for(int i = 0; i < k-1; i ++) {
-				int num = que.poll();
-				que.offer(num);
-			}
-			
-			ans[index] = que.poll();
-			index++;
 
+		int index = 0;
+		while(list.size() != 0) {
+			for(int i = 0; i < k-1; i++) {
+				index++;
+				if(index >= list.size()) index %= list.size();
+			}
+			ans.add(list.get(index));
+			list.remove(index);
 		}
-		sb.append('<');
 		
-		for(int i = 0; i < ans.length - 1; i++) {
-			sb.append(ans[i]).append(", ");
-		}
-		
-		sb.append(ans[ans.length-1]).append('>');
+		sb.append('<').append(ans.toString().substring(1, ans.toString().length()-1)).append('>');
 		
 		System.out.println(sb);
-		
 		
 	}
 }
