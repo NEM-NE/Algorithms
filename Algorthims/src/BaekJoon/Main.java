bf1acb61aeb1c378634046e418afac5234ea3298
NEMNE <dlatqdlatq@naver.com>
2021-04-28 20:49:58 +0900

    0428 10799 1406 1158 2609 1850 9613 solve

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,40 +4,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 44
-	
+	// 8 : 28 20min
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
 	
+		int n = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
 		if(b == 10) {
-			System.out.println(a);
+			System.out.println(n);
 			System.exit(0);
 		}
 		
-		int rem = a % b;
-		while(a > b) {
-			a /= b;
-			if(a > 10) {
-				sb.append((char)(54+b));
+		int m = 0;
+		int num = n+1;
+		while(num >= b) {
+			num /= b;
+			m++;
+		}
+		
+		int r = (n+1)%b;
+		
+		for(int i = 0; i < m; i++) {
+			if(b > 10) {
+				sb.append((char)('6' + b));
 			}else {
-				sb.append(a);
+				sb.append((char)('/' + b));
 			}
 		}
 		
-		if(rem < 10) {
-			sb.append((char)(rem+47));
-		}else {
-			sb.append((char)(rem+55));
+		if(b > 10 && r > 0) {
+			if(r >= 10) {
+				sb.append('7' + b);
+			}else {
+				sb.append('0' + b);
+			}
+			
+		}else if(r > 0) {
+			sb.append('0' + b);
 		}
-
 		
 		System.out.println(sb);
+		
 	}
 	
+
 }
