2285d66506482f424dd14c5968cce4b4997a370b
NEMNE <dlatqdlatq@naver.com>
2021-05-02 00:09:31 +0900

    0502 Mathematics 2089 11576 6588 11653 10872 1676

--- Algorthims/src/BaekJoon/Ex1676.java
+++ Algorthims/src/BaekJoon/Ex1676.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Ex1676 {
 	// 11 : 41
 	
 	public static void main(String[] args) throws IOException {		
--- Algorthims/src/BaekJoon/Ex2089.java
+++ Algorthims/src/BaekJoon/Ex2089.java
@@ -3,30 +3,31 @@ package BaekJoon;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	// 11 : 41
+public class Ex2089 {
+	// 9 : 57
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int num = Integer.parseInt(br.readLine());
 		
-		int cnt = 0;
-		int a = num % 5;
-		num -= a;
-		for(int j = 5; j <= num; j+=5) {
-			int b = j;
-			while((b/5)%5 == 0) {
-				cnt++;
-				b /= 5;
-			}
+		if(num == 0) {
+			System.out.println(num);
+			System.exit(0);
 		}
-		cnt += num/5;
 		
-		sb.append(cnt);
+		while(num != 0) {
+			int k = Math.abs(num%-2);
+			sb.append(k);
+			num = (int) Math.ceil((double)num/-2);
+		}
+		
+		sb.reverse();
 		System.out.println(sb);
+			
+
 	}
-	
 	
 
 }
