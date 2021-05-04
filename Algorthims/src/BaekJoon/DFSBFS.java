fd3921e59758da5422e5dc3349f4ff79b79e6bfb
NEMNE <dlatqdlatq@naver.com>
2021-05-05 00:04:39 +0900

    0505 dfs&bfs study..

--- Algorthims/src/BaekJoon/DFSBFS.java
+++ Algorthims/src/BaekJoon/DFSBFS.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.io.*;
 import java.util.*;
 
-public class Test {
+public class DFSBFS {
 
 	public static boolean[] visited = new boolean[9];
 	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
@@ -15,6 +15,23 @@ public class Test {
 		
 		for(int i = 0; i < graph.get(x).size(); i++) {
 			if(!visited[graph.get(x).get(i)]) dfs(graph.get(x).get(i));
+		}
+	}
+	
+	public static void bfs(int start) {
+		Queue<Integer> que = new LinkedList<>();
+		que.offer(start);
+		visited[start] = true;
+		
+		while(!que.isEmpty()) {
+			int index = que.poll();
+			str += index + " ";
+			for(int i = 0; i < graph.get(index).size(); i++) {
+				if(!visited[graph.get(index).get(i)]) {
+					que.add(graph.get(index).get(i));
+					visited[graph.get(index).get(i)] = true;
+				}
+			}
 		}
 	}
 	
@@ -50,7 +67,7 @@ public class Test {
 		graph.get(8).add(1);
 		graph.get(8).add(7);
 		
-		dfs(1);
+		bfs(1);
 		System.out.println(str);
     }
 
