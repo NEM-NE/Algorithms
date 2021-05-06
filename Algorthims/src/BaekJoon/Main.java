e59cac8a71225d26996a6c27cbde28c6d469865d
NEMNE <dlatqdlatq@naver.com>
2021-05-07 00:03:05 +0900

    0507 graph 2004 1260 11724 1707 solve

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,41 +4,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 41	
+	// 11 : 11	
+	
+	static int[][]graph;
+	static boolean[] visited;
+	static int[] cnt;
+	
+	static void dfs(int x) {
+		visited[x] = true;
+		cnt[x]++;
+		for(int i = 1; i < graph[x].length; i++) {
+			if(!visited[i] && graph[x][i] == 1) dfs(i);
+			else if(visited[i] && graph[x][i] == 1)
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
 		
 		int tc = Integer.parseInt(br.readLine());
-		
-		st = new StringTokenizer(br.readLine());
-		
-		int sum = 0;
 		for(int i = 0; i < tc; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			sum += num * Math.pow(a, tc-1-i);
-		}
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		while(sum > 0) {
-			int num = sum%b;
-			list.add(num);
-			sum /= b;
-		}
-		
-		for(int i = 0; i < list.size(); i++) {
-			sb.append(list.get(list.size()-1-i)).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int v = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 			
+			graph = new int[v+1][v+1];
+			visited = new boolean[v+1];
+			cnt = new int[v+1];
+			
+			for(int j = 0; j < e; j++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+
+				graph[a][b] = 1;
+				graph[b][a] = 1;
+			}
+			
+			dfs(1);
+			
+			boolean isBipartie = true;
+			for(int j = 1; j < visited.length; j++) {
+				if(visited[j] != 1) {
+					isBipartie = false;
+					break;
+				}
+			}
+			if(isBipartie) sb.append("YES").append('\n');
+			else sb.append("NO").append('\n');
 		}
 		
 		System.out.println(sb);
-		
-		
 	}
 	
 }
--- Algorthims/src/BaekJoon/Test.java
+++ Algorthims/src/BaekJoon/Test.java
@@ -4,54 +4,37 @@ import java.io.*;
 import java.util.*;
 
 public class Test {
+    static int[] dp = new int[9];
+    public static void main(String[] args) {
+        int[] ary = new int[9];
+        //input data
+        ary[0] = 2;
+        ary[1] = 1;
+        ary[2] = 5;
+        ary[3] = 4;
+        ary[4] = 3;
+        ary[5] = 7;
+        ary[6] = 6;
+        ary[7] = 8;
+        ary[8] = 9;
 
-	public static boolean[] visited = new boolean[9];
-	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-	static String str = "";
-	
-	public static void dfs(int x) {
-		visited[x] = true;
-		str += x + " ";
-		
-		for(int i = 0; i < graph.get(x).size(); i++) {
-			if(!visited[graph.get(x).get(i)]) dfs(graph.get(x).get(i));
-		}
-	}
-	
-	public static void main(String[] args) {
-		for(int i =0; i < 9; i++) {
-			graph.add(new ArrayList<Integer>());
-		}
-		
-		// add 
-		graph.get(1).add(2);
-		graph.get(1).add(3);
-		graph.get(1).add(8);
-		
-		graph.get(2).add(1);
-		graph.get(2).add(7);
-		
-		graph.get(3).add(1);
-		graph.get(3).add(4);
-		graph.get(3).add(5);
-		
-		graph.get(4).add(3);
-		graph.get(4).add(5);
-		
-		graph.get(5).add(3);
-		graph.get(5).add(4);
-		
-		graph.get(6).add(7);
-		
-		graph.get(7).add(2);
-		graph.get(7).add(6);
-		graph.get(7).add(8);
-		
-		graph.get(8).add(1);
-		graph.get(8).add(7);
-		
-		dfs(1);
-		System.out.println(str);
+        //LIS algorithms
+        for(int i = 1; i < 9; i++){
+            dp[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(ary[i] > ary[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        //find max LIS num
+        int max = 0;
+        for(int i = 0; i < dp.length; i++){
+            if(max < dp[i]) max = dp[i];
+        }
+
+        System.out.println(max);
     }
 
 
