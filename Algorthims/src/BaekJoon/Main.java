b89e1b9666b07f1e2ff0c5c0a144985e0667193d
NEMNE <dlatqdlatq@naver.com>
2021-05-09 00:11:56 +0900

    0509 Graph 1707 10451 9466 solved

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,42 +4,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 11 : 25	
+	// 10 : 55
+	static boolean[] visited;
+	static int[] graph;
+	static int start;
+	static int cnt;
+	static ArrayList<Integer> cycleList = new ArrayList<>();
+	
+	static boolean dfs(int x) {
+		if(cycleList.contains(x)) return false;
+		if(x == start) return true;
+		visited[x] = true;
+
+		if(!visited[graph[x]]) {
+			if(dfs(graph[x])) {
+				cycleList.add(graph[x]);
+				return true;
+			}
+			return false;
+		}
+		
+		return false;
+	}
 	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int p = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		list.add(a);
-		
-		int index = 0;
-		while(true) {
-			int num = list.get(index);
-			int sum = 0;
-			while(num > 0) {
-				int n = num%10;
-				sum += Math.pow(n, p);
-				num /= 10;
+		int tc = Integer.parseInt(br.readLine());
+		for(int i = 0; i < tc; i++) {
+			int num = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			graph = new int[num+1];
+			cnt = 0;
+			
+			for(int j = 1; j < graph.length; j++) {
+				graph[j] = Integer.parseInt(st.nextToken());
 			}
 			
-			for(int i = 0; i < list.size(); i++) {
-				if(list.get(i) == sum) {
-					sb.append(i);
-					System.out.println(sb);
-					return;
+			// default
+			
+			
+			for(int j = 1; j < graph.length; j++) {
+				visited = new boolean[num+1];
+				start = j;
+				if(!dfs(graph[j])) {
+					cnt++;
+				}else {
+					cycleList.add(graph[j]);
 				}
 			}
 			
-			list.add(sum);
-			index++;
-			
+			sb.append(cnt).append('\n');
 		}
+		
+		System.out.println(sb);
 	}
 	
 }
