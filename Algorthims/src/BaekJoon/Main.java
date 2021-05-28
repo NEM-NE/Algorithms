3c1b9fafd571193e826157f7acaa9b0a98a63f9b
NEMNE <dlatqdlatq@naver.com>
2021-05-28 17:06:18 +0900

    sovle : 1517 2448 11729 1780

--- Algorthims/src/BaekJoon/Main.java
+++ Algorthims/src/BaekJoon/Main.java
@@ -4,67 +4,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	// 10 : 38
+	// 3 : 23
 	
-	static String[] answer;
-	
-	static String pat1 = "***";
-	static String pat2 = "* *";
-	static String pat3 = "***";
-	
-	static void drawBlank(int size, int start) {
-		for(int i = 0; i < size; i++) {
-			for(int j = 0; j < size; j++) {
-				answer[start + i] += " ";
-			}
+	static int[] ary;
+	static int[] temp;
+	static int cnt;
+	public static void mergeSort(int arr[], int l, int r) {
+		if(l<r) {
+			int mid = (l+r)/2;
+			mergeSort(arr, l, mid);
+			mergeSort(arr, mid+1, r);
+			merge(arr, l, mid, r);
 		}
 	}
-	
-	static void drawPattern(int size, int start) {
-		if(size == 3) {
-			answer[start] += pat1;
-			answer[start + 1] += pat2;
-			answer[start + 2] += pat3;
-		}else {
-			drawPattern(size/3, start);
-			drawPattern(size/3, start);
-			drawPattern(size/3, start);
-			
-			drawPattern(size/3, start + size/3);
-			drawBlank(size/3, start + size/3);
-			drawPattern(size/3, start + size/3);
-			
-			drawPattern(size/3, start + 2 * size/3);
-			drawPattern(size/3, start + 2 * size/3);
-			drawPattern(size/3, start + 2 * size/3);
+	public static void merge(int arr[], int l, int mid, int r) {
+		int i = l;
+		int j = mid+1;
+		int k = l;
+		int temp[] = new int[arr.length];
+		while(i<=mid && j<=r) {
+			cnt++;
+			if(arr[i] < arr[j]) {
+				temp[k++] = arr[i++];
+			}else {
+				temp[k++] = arr[j++];
+			}
 		}
+		while(i<=mid)
+			temp[k++] = arr[i++];
+		while(j<=r)
+			temp[k++] = arr[j++];
+		
+		for(int index=l; index<k; index++)
+			arr[index] = temp[index];
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
-
+		
 		int tc = Integer.parseInt(br.readLine());
-		answer = new String[tc];
 		
+		ary = new int[tc];
+		temp = new int[tc];
+		cnt = 0;
+		
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < tc; i++) {
-			answer[i] = "";
+			ary[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		if(tc > 3) {
-			drawPattern(tc, 0);
-			
-		}else {
-			sb.append(pat1).append('\n');
-			sb.append(pat2).append('\n');
-			sb.append(pat3).append('\n');
-		}
+		mergeSort(ary, 0, tc-1);
 		
-		for(int i = 0; i < answer.length; i++) {
-			sb.append(answer[i]).append('\n');
-		}
-		
+		sb.append(cnt);
 		System.out.println(sb);
 	}
 
